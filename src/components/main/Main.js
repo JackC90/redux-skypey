feec7c19fee1622402cf35a1e810f555b7f9b7b3
jack <jack@goquo.com>
2018-08-10 18:25:52 +0800

    Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Main.css";
+import Empty from "../empty/Empty";
+import store from '../../store';
 
-const Main = () => {
-  return <main className="Main"></main>;
+const Main = ({ user, activeUserId }) => {
+  const renderMainContent = () => {
+    const { user } = store.getState();
+
+    if (!activeUserId) {
+      return <Empty user={user} activeUserId={activeUserId} />
+    } else {
+      return <main className="Main">{renderMainContent()}</main>;
+    };
+  }
+
+  return <main className="Main">{renderMainContent()}</main>;
 };
 
 export default Main;
